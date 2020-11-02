025532b09563cf21aec824ed5b8cf5ed476cbc30
ahah12k <ahah12k@gmail.com>
2020-11-02 16:56:46 +0900

    response check

--- ts-react-master/4.반응속도체크/ResponseCheckClass.tsx
+++ ts-react-master/4.반응속도체크/ResponseCheckClass.tsx
@@ -19,32 +19,34 @@ class ResponseCheck extends Component<{}, State> {
   endTime: number | null = null;
 
   onClickScreen = () => {
-    const { state } = this.state;
+    const { state } = this.state;//구조분해
     if (state === 'waiting') {
-      this.timeout = setTimeout(() => {
+      this.timeout = window.setTimeout(() => {
         this.setState({
           state: 'now',
           message: '지금 클릭',
         });
-        this.startTime = new Date().getTime();
+        this.startTime = new Date().getTime();//바뀐순간 시간체크 시작
       }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
       this.setState({
         state: 'ready',
         message: '초록색이 되면 클릭하세요.',
       });
     } else if (state === 'ready') { // 성급하게 클릭
-      clearTimeout(this.timeout!);
+      if(this.timeout){
+        clearTimeout(this.timeout);
+      }
       this.setState({
         state: 'waiting',
         message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
       });
     } else if (state === 'now') { // 반응속도 체크
-      this.endTime = new Date().getTime();
+      this.endTime = new Date().getTime();//눌렀을때 시간체크
       this.setState((prevState) => {
         return {
           state: 'waiting',
           message: '클릭해서 시작하세요.',
-          result: [...prevState.result, this.endTime!, this.startTime!],
+          result: [...prevState.result, this.endTime!- this.startTime!],
         };
       });
     }
@@ -59,26 +61,26 @@ class ResponseCheck extends Component<{}, State> {
   renderAverage = () => {
     const {result} = this.state;
     return result.length === 0
-      ? null
-      : <>
-        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-        <button onClick={this.onReset}>리셋</button>
-      </>
+        ? null
+        : <>
+          <div>평균 시간: {(result.reduce((a, c) => a + c) / result.length).toFixed(4)}ms</div>
+          <button onClick={this.onReset}>리셋</button>
+        </>
   };
 
   render() {
     const { state, message } = this.state;
     return (
-      <>
-        <div
-          id="screen"
-          className={state}
-          onClick={this.onClickScreen}
-        >
-          {message}
-        </div>
-        {this.renderAverage()}
-      </>
+        <>
+          <div
+              id="screen"
+              className={state}
+              onClick={this.onClickScreen}
+          >
+            {message}
+          </div>
+          {this.renderAverage()}
+        </>
     )
   }
 }
