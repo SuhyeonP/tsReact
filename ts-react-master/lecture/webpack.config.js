6f5d5f4de57a33c3abe50bbb9c0d5c5495b3e068
ahah12k <ahah12k@gmail.com>
2020-11-02 12:20:02 +0900

    first

---
+++ ts-react-master/lecture/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval', // hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+    }],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+  },
+};
